b6da4a26152c035bc294de43a2debdada5bd7c77
Agrover112 <42321810+Agrover112@users.noreply.github.com>
2020-08-06 02:00:14 +0530

    Reformatted code for LoadModel 🛠️
    Reformatting of DL side logic complete onto restructuring.

--- LoadModel.java
+++ LoadModel.java
@@ -1,5 +1,6 @@
 
 package org.deeplearning4j.examples.dataexamples;
+
 import org.deeplearning4j.examples.dataexamples.BreastCancer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 
@@ -12,8 +13,30 @@ public class LoadModel {
             MultiLayerNetwork restored = MultiLayerNetwork.load(BreastCancer.locationToSave, true);
 
             System.out.println("Saved and loaded parameters are equal:      " + BreastCancer.model.params().equals(restored.params()));
-             System.out.println("Saved and loaded configurations are equal:  " + BreastCancer.model.getLayerWiseConfigurations().equals(restored.getLayerWiseConfigurations()));
+            System.out.println("Saved and loaded configurations are equal:  " + BreastCancer.model.getLayerWiseConfigurations().equals(restored.getLayerWiseConfigurations()));
+        } catch (Exception e) {
+            e.getMessage();
         }
-        catch (Exception e) { e.getMessage();}
+    }
+}
+
+package org.deeplearning4j.examples.dataexamples;
+
+import org.deeplearning4j.examples.dataexamples.BreastCancer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+
+import java.io.IOException;
+
+public class LoadModel {
+    public static void main(String args[]) {
+        try {
+            BreastCancer.main(args);
+            MultiLayerNetwork restored = MultiLayerNetwork.load(BreastCancer.locationToSave, true);
+
+            System.out.println("Saved and loaded parameters are equal:      " + BreastCancer.model.params().equals(restored.params()));
+            System.out.println("Saved and loaded configurations are equal:  " + BreastCancer.model.getLayerWiseConfigurations().equals(restored.getLayerWiseConfigurations()));
+        } catch (Exception e) {
+            e.getMessage();
+        }
     }
 }
