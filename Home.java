7f12a6423edfb5cd0cd423a768925768d39e4af5
Agrover112 <42321810+Agrover112@users.noreply.github.com>
2020-08-06 01:45:23 +0530

    Reformatting✨

--- Home.java
+++ Home.java
@@ -1,57 +1,105 @@
-import java.util.*;
+/**
+ *Text genereted by Simple GUI Extension for BlueJ
+ */
+import javax.swing.UIManager.LookAndFeelInfo;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+import javax.swing.border.Border;
+import javax.swing.ListSelectionModel;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.event.*;
+
+public class HomeGUI extends JFrame  {
+    private JMenuBar menuBar;
+    private JLabel label1;
+    private JList list1;
+
+    //Constructor 
+    public HomeGUI() {
+
+        this.setTitle("HOME");
+        this.setSize(500,400);
+
+        //pane with null layout
+        JPanel contentPane = new JPanel(null);
+        contentPane.setPreferredSize(new Dimension(500,400));
+        contentPane.setBackground(new Color(255,204,204));
+
+        label1 = new JLabel();
+        label1.setBounds(132,5,246,45);
+        label1.setBackground(new Color(214,217,223));
+        label1.setForeground(new Color(0,0,0));
+        label1.setEnabled(true);
+        label1.setFont(new Font("SansSerif",0,20)); 
+        label1.setText("............WELCOME................");
+        label1.setVisible(true);
+
+        DefaultListModel<String> l1= new DefaultListModel<>();
+        l1.addElement("Create a new account!");
+        l1.addElement("Existing  user");
+
+        JList<String> L=new JList<>(l1);
+        L.setBounds(165,99,250,250);
+        L.addListSelectionListener(new ListSelectionListener()
+            {
+                public void valueChanged(ListSelectionEvent e)
+                {
+                    if (e.getValueIsAdjusting() == false)
+                    {
+                        switch(L.getSelectedIndex())
+                        {
+                            case 0:
+                            User obj1=new User();
+                            obj1.NewUser();
+
+                            break;
+                            case 1:
+                            User obj2=new User();
+                            obj2.ExistingUser();
+                            break;
+
+                        }
+                    }
+                }
+            });
+        L.setBackground(new Color(255,204,204));
+        L.setForeground(new Color(0,0,0));
+        L.setEnabled(true);
+        L.setFont(new Font("sansserif",0,20));
+        L.setVisible(true);
+        //adding components to contentPane panel
+        contentPane.add(label1);
+        contentPane.add(L);
 
 
-public class Home extends User
-{
-	public static void main (String[] args){
-		try{
-			User U=new User();
-			int choice;
-			char a='n';
-			Scanner scan=new Scanner(System.in);
-			System.out.println("-----------------------------------------");
-			System.out.println("			WELCOME        			     ");
-			System.out.println("	    TO THE BANK PROGRAM				 ");
-			System.out.println("-----------------------------------------");
-			do
-			{
-				a='n';
-				System.out.println("Please enter your choice");
-				System.out.println("1. Create a new account");
-				System.out.println("2. Existing account");
-				System.out.println("3.Exit");
-				choice=scan.nextInt();
-				System.out.println("------------------------------------------------------------------------");
-				
-				if (choice>3||choice<1)
-				{
-					System.out.println("\n OOPS! You have entered wrong information");
-					System.out.println("-----------------------------------------------------------------------");
-					System.out.println("Do you wish to continue? [Y/N]");
-					a=scan.next().charAt(0);
-					System.out.println("-----------------------------------------------------------------------");
-				}
-			}while(a=='Y'||a=='y');
-			
-			switch(choice)
-			{
-				case 1: U.NewUser();
-						break;
-				case 2: U.ExistingUser();
-						break;
-				case 3: System.out.println("THANK YOU FOR VISITING! WE WISH TO SEE YOU AGAIN!");
-						break;
-				default: System.out.println("Invalid choice");
-			}
-		}
-		catch (InputMismatchException a)
-		{
-			System.out.println("---------------------------------------------------------------------------");
-			System.out.println("                         ERROR! WRONG INPUT                                ");
-			System.out.println("---------------------------------------------------------------------------");
-		}
-		catch(Exception e){}
-	}
+        //adding panel to JFrame and seting of window position and close operation
+        this.add(contentPane);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e)
+    {
+
+    }
+
+    public static void main(String[] args){
+        System.setProperty("swing.defaultlaf", "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    new HomeGUI();
+                }
+            });
+    }
+
 }
-						
-				
